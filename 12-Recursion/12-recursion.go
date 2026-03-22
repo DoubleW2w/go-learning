adaaa4ba2de139d9483845aab3abeb8c07e870b3
DoubleW2w <1049951363@qq.com>
2026-03-22 12:32:12 +0800

    docs: Add 12-recursion

---
+++ 12-Recursion/12-recursion.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 7
+	fmt.Printf("fact(%d) = ", n)
+	result := fact(n)
+	fmt.Printf(" = %d\n", result)
+
+	// fib 函数递归
+	var fib func(n int) int
+	fib = func(n int) int {
+		if n <= 1 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
+}
+
+func fact(n int) int {
+	if n == 1 {
+		fmt.Print("1")
+		return 1
+	}
+	fmt.Printf("%d + ", n)
+	return n + fact(n-1)
+}
